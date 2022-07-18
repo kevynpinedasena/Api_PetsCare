cd3ef62d62ff17c4b9cab0fbb6c489857179ee9c
juan david marin <juaanmarin2007@gmail.com>
2022-07-18 09:22:34 -0500

    agrego la clase securiry config

---
+++ src/main/java/com/pets1/app/config/securityConfig.java
@@ -0,0 +1,50 @@
+package com.pets1.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class securityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll().anyRequest()
+				.authenticated().and().httpBasic();
+	}
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(UserDetailService).passwordEncoder(passwordEncoder());
+//		
+//	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails juan = User.builder().username("juan").password(passwordEncoder().encode("juan1234")).roles("USER").build();
+		
+		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+		
+		return new InMemoryUserDetailsManager(juan, admin);
+	}
+
+	
+}
